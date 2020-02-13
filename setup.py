14d6f1fe3d95ae2f65bb72e1b8b8f0aef604f78e
Taro Kiritani <taro.kiritani@exwzd.com>
2020-02-13 05:13:58 +0000

    Add setup.py
    The repo can be easily installed with pip.

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="senet",
+    version="1.0.0",
+    python_requires=">3.6",
+    author="Taro Kiritani",
+    author_email="taro.kiritani@exwzd.com",
+    description="used for Kiritani, 2020",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=["Programming Language :: Python :: 3.6",],
+)
